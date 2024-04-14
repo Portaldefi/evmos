f2f0ff89411ba9816ef72b3e0a3f222a74a6e21f
Free Bird <freebird@Frees-MacBook-Pro.local>
2024-04-14 21:08:19 +0300

    fix testutils-network

--- testutil/network/network.go
+++ testutil/network/network.go
@@ -41,7 +41,6 @@ import (
 	tmclient "github.com/cometbft/cometbft/rpc/client"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
-	"github.com/evmos/ethermint/app"
 	"github.com/spf13/cobra"
 	"google.golang.org/grpc"
 
@@ -64,14 +63,14 @@ import (
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	"github.com/cosmos/cosmos-sdk/x/genutil"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+	simapp "github.com/evmos/ethermint/app"
 	"github.com/evmos/ethermint/app/params"
 	"github.com/evmos/ethermint/crypto/hd"
-	"github.com/evmos/evmos/v11/app"
-
 	"github.com/evmos/ethermint/encoding"
 	"github.com/evmos/ethermint/server/config"
 	ethermint "github.com/evmos/ethermint/types"
 	evmtypes "github.com/evmos/ethermint/x/evm/types"
+	"github.com/evmos/evmos/v11/app"
 )
 
 // package-wide network lock to only allow one test network at a time
@@ -90,25 +89,25 @@ type Config struct {
 	InterfaceRegistry codectypes.InterfaceRegistry
 	TxConfig          client.TxConfig
 	AccountRetriever  client.AccountRetriever
-	AppConstructor    AppConstructor   // the ABCI application constructor
-	GenesisState      app.GenesisState // custom gensis state to provide
-	TimeoutCommit     time.Duration    // the consensus commitment timeout
-	AccountTokens     math.Int         // the amount of unique validator tokens (e.g. 1000node0)
-	StakingTokens     math.Int         // the amount of tokens each validator has available to stake
-	BondedTokens      math.Int         // the amount of tokens each validator stakes
-	NumValidators     int              // the total number of validators to create and bond
-	ChainID           string           // the network chain-id
-	BondDenom         string           // the staking bond denomination
-	MinGasPrices      string           // the minimum gas prices each validator will accept
-	PruningStrategy   string           // the pruning strategy each validator will have
-	SigningAlgo       string           // signing algorithm for keys
-	RPCAddress        string           // RPC listen address (including port)
-	JSONRPCAddress    string           // JSON-RPC listen address (including port)
-	APIAddress        string           // REST API listen address (including port)
-	GRPCAddress       string           // GRPC server listen address (including port)
-	EnableTMLogging   bool             // enable Tendermint logging to STDOUT
-	CleanupDir        bool             // remove base temporary directory during cleanup
-	PrintMnemonic     bool             // print the mnemonic of first validator as log output for testing
+	AppConstructor    AppConstructor      // the ABCI application constructor
+	GenesisState      simapp.GenesisState // custom gensis state to provide
+	TimeoutCommit     time.Duration       // the consensus commitment timeout
+	AccountTokens     math.Int            // the amount of unique validator tokens (e.g. 1000node0)
+	StakingTokens     math.Int            // the amount of tokens each validator has available to stake
+	BondedTokens      math.Int            // the amount of tokens each validator stakes
+	NumValidators     int                 // the total number of validators to create and bond
+	ChainID           string              // the network chain-id
+	BondDenom         string              // the staking bond denomination
+	MinGasPrices      string              // the minimum gas prices each validator will accept
+	PruningStrategy   string              // the pruning strategy each validator will have
+	SigningAlgo       string              // signing algorithm for keys
+	RPCAddress        string              // RPC listen address (including port)
+	JSONRPCAddress    string              // JSON-RPC listen address (including port)
+	APIAddress        string              // REST API listen address (including port)
+	GRPCAddress       string              // GRPC server listen address (including port)
+	EnableTMLogging   bool                // enable Tendermint logging to STDOUT
+	CleanupDir        bool                // remove base temporary directory during cleanup
+	PrintMnemonic     bool                // print the mnemonic of first validator as log output for testing
 }
 
 // DefaultConfig returns a sane default configuration suitable for nearly all
